bfe65405a7dae4e7d5d1df3295d778af8bcde518
hap-py-python <117331957+hap-py-python@users.noreply.github.com>
2022-11-03 23:04:56 -0400

    Add files via upload

---
+++ hello_world_list.py
@@ -0,0 +1,32 @@
+
+hello = []
+
+hello.append('h')
+hello.append('e')
+hello.append('l')
+hello.append('l')
+hello.insert(4, 'o')
+
+world = []
+
+world.insert(0,'d')
+world.insert(1,'l')
+world.append('r')
+world.append('o')
+world.append('w')
+
+world.reverse()
+
+for h in hello:
+    print(h)
+print('\n')
+
+for w in world:
+    print(w)
+
+
+
+
+
+
+
